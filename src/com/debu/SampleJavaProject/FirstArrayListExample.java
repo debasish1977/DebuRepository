ae33c45fef3215eaef68ff32bdd104830174f9cf
Debasish Chakraborty <debasish1977@gmail.com>
2017-08-17 18:43:08 -0400

    first commit

---
+++ src/com/debu/SampleJavaProject/FirstArrayListExample.java
@@ -0,0 +1,23 @@
+package com.debu.SampleJavaProject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class FirstArrayListExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> firstArr = new ArrayList<String>();
+		firstArr.add("Debu");
+		firstArr.add("Nilu");
+		firstArr.add("Rivi");
+		firstArr.add("Rey");
+		Iterator<String> firstIte=firstArr.iterator();
+		while(firstIte.hasNext())
+		{
+			System.out.println("Elements-->"+firstIte.next());
+		}
+
+	}
+
+}
